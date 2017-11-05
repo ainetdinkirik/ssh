eddf95e9f648f5f352d6036f2a163696af2787f8
ainetdinkirik <kirik.ainetdin@gmail.com>
2017-11-05 16:39:52 +0200

    Korjailukokeilua

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,18 @@
 class ssh {
-	package {'ssh':
-		ensure => installed,
-		allowcdrom => 'true'
-	}
+    package { 'openssh-server':
+            ensure => 'installed',
+		allowcdrom => true,
+    }
 
-	file {'/etc/ssh/sshd_config':
-		content => template ("ssh/sshd_config"),
-		require => Package ['ssh'],
-		notify => Service ['ssh'],
-	}
+     file { '/etc/ssh/sshd_config':
+            content => template('ssh/sshd_config'),
+            require => Package['openssh-server'],
+            notify => Service['ssh'],
+    }
 
-	service {'ssh':
-		ensure => running,
-		enable => true,
-		require => Package['ssh'],
-	}
+    service { 'ssh':
+            ensure => 'running',
+            enable => 'true',
+            require => Package['openssh-server'],
+    }
 }
